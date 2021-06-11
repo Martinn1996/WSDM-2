6763c384eea891012b76e8d9e8e626271653144f
Quentin Lee <Quentin.c.l.lee@gmail.com>
2021-06-11 10:26:33 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 kubectl create namespace cloudstate
 
-kubectl apply -f https://raw.githubusercontent.com/cloudstateio/cloudstate/v0.6.0/operator/cloudstate.yaml
+kubectl apply -n cloudstate -f https://raw.githubusercontent.com/cloudstateio/cloudstate/v0.6.0/operator/cloudstate.yaml
 
-kubectl create -f api/deployment.yml --save-config
-kubectl create -f order_service/deployment.yml --save-config
-kubectl create -f payment_service/deployment.yml --save-config
-kubectl create -f stock_service/deployment.yml --save-config
+kubectl create -n cloudstate -f api/deployment.yml --save-config
+kubectl create -n cloudstate -f order_service/deployment.yml --save-config
+kubectl create -n cloudstate -f payment_service/deployment.yml --save-config
+kubectl create -n cloudstate -f stock_service/deployment.yml --save-config
 kubectl expose deployment cloudstate-api --type="LoadBalancer"
 
